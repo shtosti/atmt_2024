5a4d931862d1f75929ab888bf549499bce735b6b
shtosti <hanna.hubarava@gmail.com>
2024-12-08 18:41:43 +0100

    run all: increment beam by 3

--- evaluate_5.sh
+++ evaluate_5.sh
@@ -8,8 +8,18 @@
 # | sacrebleu data/en-fr/raw/test.en
 
 
-### small beam-5 ###
-OUTPUT_DIR="assignments/05/small--beam-size-5"
-cat \
-$OUTPUT_DIR/translations.p.txt \
-| sacrebleu data/en-fr/raw/test.en
+# Starting beam size
+START_BEAM=2
+# Ending beam size
+END_BEAM=25
+# Beam increment
+INCREMENT=3
+
+# Loop over the beam sizes
+for BEAM_SIZE in $(seq $START_BEAM $INCREMENT $END_BEAM)
+do
+    OUTPUT_DIR="assignments/05/large--beam-size-$BEAM_SIZE"
+    cat \
+    $OUTPUT_DIR/translations.p.txt \
+    | sacrebleu data/en-fr/raw/test.en
+done
--- postprocess_5.sh
+++ postprocess_5.sh
@@ -8,8 +8,18 @@
 # $OUTPUT_DIR/translations.p.txt en
 
 
-### small beam=5 ###
-OUTPUT_DIR="assignments/05/small--beam-size-5"
-scripts/postprocess.sh \
-$OUTPUT_DIR/translations.txt \
-$OUTPUT_DIR/translations.p.txt en
+# Starting beam size
+START_BEAM=2
+# Ending beam size
+END_BEAM=25
+# Beam increment
+INCREMENT=3
+
+# Loop over the beam sizes
+for BEAM_SIZE in $(seq $START_BEAM $INCREMENT $END_BEAM)
+do
+    OUTPUT_DIR="assignments/05/large--beam-size-$BEAM_SIZE"
+    scripts/postprocess.sh \
+    $OUTPUT_DIR/translations.txt \
+    $OUTPUT_DIR/translations.p.txt en
+done
